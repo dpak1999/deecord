c6ced01d87302c25228e31b4eebed4a20fccd964
Deepak <deepakkumardash313@gmail.com>
2023-09-20 23:34:28 +0530

    add navigation user servers

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -3,6 +3,9 @@ import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
 import { FC } from "react";
 import NavigationAction from "./navigation-action";
+import { Separator } from "../ui/separator";
+import { ScrollArea } from "../ui/scroll-area";
+import NavigationItem from "./navigation-item";
 
 interface NavigationSidebarProps {}
 
@@ -26,6 +29,14 @@ const NavigationSidebar: FC<NavigationSidebarProps> = async ({}) => {
   return (
     <div className="space-y-4 flex flex-col items-center h-full text-primary w-full py-3 dark:bg-[#1e1f22]">
       <NavigationAction />
+      <Separator className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto" />
+      <ScrollArea className="w-full flex-1">
+        {server.map((s) => (
+          <div key={s.id} className="mb-4">
+            <NavigationItem id={s.id} imageUrl={s.imageUrl} name={s.name} />
+          </div>
+        ))}
+      </ScrollArea>
     </div>
   );
 };
