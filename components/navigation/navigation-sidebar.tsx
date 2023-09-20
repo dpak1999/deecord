5a18b2b19575983f3e26c7f80e3d9cc6cafc0772
Deepak <deepakkumardash313@gmail.com>
2023-09-20 23:39:27 +0530

    add navigation sidebar users and mode toggle

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -6,6 +6,8 @@ import NavigationAction from "./navigation-action";
 import { Separator } from "../ui/separator";
 import { ScrollArea } from "../ui/scroll-area";
 import NavigationItem from "./navigation-item";
+import { ModeToggle } from "../mode-toggel";
+import { UserButton } from "@clerk/nextjs";
 
 interface NavigationSidebarProps {}
 
@@ -37,6 +39,13 @@ const NavigationSidebar: FC<NavigationSidebarProps> = async ({}) => {
           </div>
         ))}
       </ScrollArea>
+      <div className="pb-3 mt-auto flex items-center flex-col gap-y-4">
+        <ModeToggle />
+        <UserButton
+          afterSignOutUrl="/"
+          appearance={{ elements: { avatarBox: "h-[48px] w-[48px]" } }}
+        />
+      </div>
     </div>
   );
 };
